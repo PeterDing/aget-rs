b7aca146631f42fe91b7c7fa2a9d6c508295bd77
PeterDing <dfhayst@gmail.com>
2019-04-16 13:07:48 +0800

    Add mutable static `DEBUG`, `QUIET` for global using; Add `debug!`

--- src/util.rs
+++ src/util.rs
@@ -112,8 +112,28 @@ impl TimeOfFmt for u64 {
     }
 }
 
+pub static mut DEBUG: bool = false;
+pub static mut QUIET: bool = false;
+
 macro_rules! print_err {
     ( $ctx:expr, $err:expr ) => {
         eprintln!("[{}:{}] {}: {}", file!(), line!(), $ctx, $err);
+    };
+}
+
+macro_rules! debug {
+    ( $title:expr, $msg:expr ) => {
+        unsafe {
+            if crate::util::DEBUG {
+                eprintln!("[{}:{}] {}: {:#?}", file!(), line!(), $title, $msg);
+            }
+        }
+    };
+    ( $title:expr ) => {
+        unsafe {
+            if crate::util::DEBUG {
+                eprintln!("[{}:{}] {}", file!(), line!(), $title);
+            }
+        }
     };
 }
