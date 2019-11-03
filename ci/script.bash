f586525740d8c6f6330d34ba787e15a72a4ae01f
PeterDing <dfhayst@gmail.com>
2019-11-03 10:31:55 +0800

    Update test url

--- ci/script.bash
+++ ci/script.bash
@@ -7,14 +7,14 @@ cargo build --target "$TARGET" --verbose
 
 cargo test --target "$TARGET" --verbose
 
-RUST_BACKTRACE=1 cargo run --target "$TARGET" -- http://cdimage.ubuntu.com/ubuntu-base/releases/18.10/release/ubuntu-base-18.10-base-amd64.tar.gz
+RUST_BACKTRACE=1 cargo run --target "$TARGET" -- http://cdimage.ubuntu.com/ubuntu-base/releases/18.04/release/ubuntu-base-18.04-base-amd64.tar.gz
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]
 then
-    if [ "$(cat ubuntu-base-18.10-base-amd64.tar.gz | md5sum | cut -f1 -d' ')" != "a1a02c6fd451aa80f3ed28913a91bdcf" ]; then exit 1; fi
+    if [ "$(cat ubuntu-base-18.04-base-amd64.tar.gz | md5sum | cut -f1 -d' ')" != "2ed358ddea75aaa2aae924f2a4bc0aa0" ]; then exit 1; fi
 elif [ "$TRAVIS_OS_NAME" = "osx" ]
 then
-    if [ "$(cat ubuntu-base-18.10-base-amd64.tar.gz | md5 | cut -f1 -d' ')" != "a1a02c6fd451aa80f3ed28913a91bdcf" ]; then exit 1; fi
+    if [ "$(cat ubuntu-base-18.04-base-amd64.tar.gz | md5 | cut -f1 -d' ')" != "2ed358ddea75aaa2aae924f2a4bc0aa0" ]; then exit 1; fi
 fi
 
-rm ubuntu-base-18.10-base-amd64.tar.gz
+rm ubuntu-base-18.04-base-amd64.tar.gz
