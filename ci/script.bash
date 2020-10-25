e8d829d4909eef2e76c5379a2e833be62b7cf0b6
PeterDing <dfhayst@gmail.com>
2020-10-25 13:08:08 +0800

    Update ci

--- ci/script.bash
+++ ci/script.bash
@@ -7,14 +7,20 @@ cargo build --target "$TARGET" --verbose
 
 cargo test --target "$TARGET" --verbose
 
-RUST_BACKTRACE=1 cargo run --target "$TARGET" -- http://cdimage.ubuntu.com/ubuntu-base/releases/18.04/release/ubuntu-base-18.04-base-amd64.tar.gz
+TEST_URI="https://github.com/mpv-player/mpv/archive/49d6a1e77d3dba5cf30a2cb9cd05dc6dd6407fb2.zip"
+TEST_MD5="703cd17daf71138b90622d52fe6fb6a5"
+TEST_NAME="49d6a1e77d3dba5cf30a2cb9cd05dc6dd6407fb2.zip"
+
+RUST_BACKTRACE=1 cargo run --target "$TARGET" -- $TEST_URI
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]
 then
-    if [ "$(cat ubuntu-base-18.04-base-amd64.tar.gz | md5sum | cut -f1 -d' ')" != "2ed358ddea75aaa2aae924f2a4bc0aa0" ]; then exit 1; fi
+    md5="$(cat $TEST_NAME | md5sum | cut -f1 -d' ')"
 elif [ "$TRAVIS_OS_NAME" = "osx" ]
 then
-    if [ "$(cat ubuntu-base-18.04-base-amd64.tar.gz | md5 | cut -f1 -d' ')" != "2ed358ddea75aaa2aae924f2a4bc0aa0" ]; then exit 1; fi
+    md5="$(cat $TEST_NAME | md5 | cut -f1 -d' ')"
 fi
 
-rm ubuntu-base-18.04-base-amd64.tar.gz
+if [ "$md5" != "$TEST_MD5" ]; then exit 1; fi
+
+rm $TEST_NAME
