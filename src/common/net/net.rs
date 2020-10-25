72f18243117818dca233e3a6c21989421ad9c756
PeterDing <dfhayst@gmail.com>
2020-10-25 12:19:45 +0800

    Fix `join_uri`

--- src/common/net/net.rs
+++ src/common/net/net.rs
@@ -229,7 +229,7 @@ pub async fn request(
 }
 
 pub fn join_uri(base_uri: &Uri, uri: &str) -> Result<Uri> {
-    let new_uri: Uri = if uri.starts_with("/") {
+    let new_uri: Uri = if !uri.to_lowercase().starts_with("http") {
         let base_url = Url::parse(&format!("{}", base_uri))?;
         base_url.join(uri)?.as_str().parse()?
     } else {
