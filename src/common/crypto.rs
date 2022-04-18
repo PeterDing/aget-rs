ea106e1f4029cf61bc65af23010461cb38a10128
PeterDing <dfhayst@gmail.com>
2022-04-18 19:22:08 +0800

    Use aes and cbc from Rust Crypto

--- src/common/crypto.rs
+++ src/common/crypto.rs
@@ -1,8 +1,10 @@
-use openssl::symm::{decrypt, Cipher};
+use aes::cipher::{block_padding::Pkcs7, BlockDecryptMut, KeyIvInit};
 
 use crate::common::errors::Result;
 
+type Aes128CbcDec = cbc::Decryptor<aes::Aes128>;
+
 pub fn decrypt_aes128(key: &[u8], iv: &[u8], enc: &[u8]) -> Result<Vec<u8>> {
-    let cipher = Cipher::aes_128_cbc();
-    Ok(decrypt(cipher, key, Some(iv), enc)?)
+    let cipher = Aes128CbcDec::new(key.into(), iv.into());
+    Ok(cipher.decrypt_padded_vec_mut::<Pkcs7>(enc)?)
 }
